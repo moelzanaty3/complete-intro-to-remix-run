afac8fc5c83a738edfa5952511ab51c72d99a545
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-05-15 16:47:07 +0200

    feat: loading data

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,47 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+type LoaderData = {
+  posts: Post[];
+};
+
+// Loaders are the backend "API" for their component
+// it's already wired up for you through useLoaderData.
+export const loader = async () => {
+  return json<LoaderData>({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  });
+};
+
 export default function Posts() {
+  const { posts } = useLoaderData() as LoaderData;
+  console.log(posts);
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-slate-300 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
---
+++ notes.md
@@ -0,0 +1,11 @@
+# Notes
+
+## Loading Data
+
+- Data loading is built into Remix.
+
+- If your web dev background is primarily in the last few years, you're probably used to creating two things here: an API route to provide data and a frontend component that consumes it.
+
+- In Remix your frontend component is also its own API route and it already knows how to talk to itself on the server from the browser. That is, you don't have to fetch it.
+
+- If you have your server and browser consoles both open, you'll note that they both logged our post data. That's because Remix rendered on the server to send a full HTML document like a traditional web framework, but it also hydrated in the client and logged there too.
